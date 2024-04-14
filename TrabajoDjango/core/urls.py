7c6504d3cada4dc6a5333417d0f66635e5829bc5
felipemat6767 <felipe_6767@hotmail.es>
2024-04-14 00:35:55 -0400

    14-4

--- TrabajoDjango/core/urls.py
+++ TrabajoDjango/core/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views  import  listado_juegos, detalle_juegos , detalle_juego,crear_juegos, editar_juegos, Home
+from .views  import Home, listado_libros, detalle_libro,detalle_libros, crear_libros, editar_libros
 
+  
 urlpatterns = [ 
-    
-    path('',  Home, name="Home"), 
-    path('juegosLista',  listado_juegos, name="listado_juegos"), 
-    path('juegos/categoria/<int:id>/',  detalle_juegos , name="detalle_juegos"),
-     path('juego/detalles/<int:id>/',  detalle_juego , name="detalle_juego"),
-     path('juego/crearJuego/',crear_juegos, name="crear_juegos"),
-     path('juego/<int:id>/editar/',editar_juegos, name="editar_juegos"),
+    path('' , Home, name="listado_libros"),   
+    path('librosLista',  listado_libros, name="listado_libros"), 
+    path('libro/categoria/<int:id>/',  detalle_libros , name="detalle_libros"),
+     path('libro/detalles/<int:id>/',  detalle_libro , name="detalle_libro"),
+     path('libro/crearLibro/',crear_libros, name="crear_libros"),
+     path('libro/<int:id>/editar/',editar_libros, name="editar_libros"),
 ] 
